9ee3d89c4080e23575d08de322912c83922add08
Jason Lieb <Jason.lieb@outlook.com>
2022-11-12 13:43:05 -0500

    create button to add books, create function to add form and style with css

--- script.js
+++ script.js
@@ -17,11 +17,6 @@ Book.prototype.info = function() {
   return this.title + ', ' + this.author + ', ' + this.pages + ', ' + this.read;
 }
 
-function addBookToLibrary(book) {
-  library.push(book);
-  displayLibrary();
-}
-
 function bookToTableEntry(book) {
   let { title, author, pages, read } = book;
   return `<td>${title}</td><td>${author}</td><td>${pages}</td><td>${read}</td>`
@@ -39,10 +34,33 @@ function displayLibrary() {
   library.map(appendNewRow);
 }
 
+function addBookToLibrary(book) {
+  library.push(book);
+  displayLibrary();
+}
+
 function resetLibrary() {
   let table = document.querySelector('table');
   table.innerHTML = '';
   table.innerHTML = '<tr><th>Title</th><th>Author</th><th># of Pages</th><th>Read?</th></tr>';
 }
 
+let newBook = document.querySelector('#newBook');
+newBook.addEventListener('click', createForm);
+
+function createForm() {
+  let body = document.querySelector('body');
+  body.removeChild(newBook);
+  let form = document.createElement('form');
+  let formHTML = '<label name="title">Title <input type="text"/></label><label name="author">Author <input type="text"/></label>'
+      + '<label name="pages"># of Pages <input type="text"/></label><label name="read">Read? <input type="text"/></label>'
+      + '<button id="submit" type="submit">Submit</button>';
+  form.innerHTML = formHTML;
+  body.appendChild(form);
+}
+
 resetLibrary();
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);

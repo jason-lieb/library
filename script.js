16e834433d095aabe04b22f8c76dfece18045082
Jason Lieb <Jason.lieb@outlook.com>
2022-11-13 15:30:17 -0500

    refactor code

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let library = [];
-
 let body = document.querySelector('body');
+let table = document.createElement('table');
+body.appendChild(table);
+resetLibrary();
+addNewBookButton();
 
 let book1 = new Book('a','b',5,true);
 let book2 = new Book('c','d',3,true);
 let book3 = new Book('g','e',42,false);
 let book4 = new Book('n','w',7,false);
-
-resetLibrary();
-addNewBookButton();
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
@@ -26,26 +26,8 @@ Book.prototype.info = function() {
   return this.title + ', ' + this.author + ', ' + this.pages + ', ' + this.read;
 }
 
-function bookToTableEntry(book) {
-  let { title, author, pages, read } = book;
-  return `<td>${title}</td><td>${author}</td><td>${pages}</td><td>${read}</td>`
-}
-
-function appendNewRow(book) {
-  let table = document.querySelector('table');
-  let newRow = document.createElement('tr');
-  newRow.innerHTML = bookToTableEntry(book);
-  table.appendChild(newRow);
-}
-
-function displayLibrary() {
-  resetLibrary();
-  library.map(appendNewRow);
-}
-
-function addBookToLibrary(book) {
-  library.push(book);
-  displayLibrary();
+function resetLibrary() {
+  table.innerHTML = '<tr><th>Title</th><th>Author</th><th># of Pages</th><th>Read?</th></tr>';
 }
 
 function addNewBookButton() {
@@ -56,9 +38,36 @@ function addNewBookButton() {
   newBook.addEventListener('click', createForm);
 }
 
-function resetLibrary() {
-  let table = document.querySelector('table');
-  table.innerHTML = '<tr><th>Title</th><th>Author</th><th># of Pages</th><th>Read?</th></tr>';
+function addBookToLibrary(book) {
+  library.push(book);
+  displayLibrary();
+}
+
+function displayLibrary() {
+  resetLibrary();
+  library.map(appendNewRow);
+}
+
+function appendNewRow(book) {
+  let newRow = document.createElement('tr');
+  newRow.innerHTML = bookToTableEntry(book);
+  table.appendChild(newRow);
+}
+
+function bookToTableEntry(book) {
+  let { title, author, pages, read } = book;
+  return `<td>${title}</td><td>${author}</td><td>${pages}</td><td>${read}</td>`
+}
+
+function createForm() {
+  body.removeChild(newBook);
+  let form = document.createElement('form');
+  let formHTML = '<label name="title">Title <input type="text"/></label><label name="author">Author <input type="text"/></label>'
+      + '<label name="pages"># of Pages <input type="text"/></label><label name="read">Read? <input type="text"/></label>'
+      + '<button id="submit">Submit</button>';
+  form.innerHTML = formHTML;
+  body.appendChild(form);
+  form.addEventListener('submit', submitForm);
 }
 
 function submitForm(e) {
@@ -71,14 +80,7 @@ function submitForm(e) {
   addNewBookButton();
 }
 
-function createForm() {
-  let body = document.querySelector('body');
-  body.removeChild(newBook);
-  let form = document.createElement('form');
-  let formHTML = '<label name="title">Title <input type="text"/></label><label name="author">Author <input type="text"/></label>'
-      + '<label name="pages"># of Pages <input type="text"/></label><label name="read">Read? <input type="text"/></label>'
-      + '<button id="submit">Submit</button>';
-  form.innerHTML = formHTML;
-  body.appendChild(form);
-  form.addEventListener('submit', submitForm);
-}
+// Possible changes:
+// - Don't reset library everytime a book is added
+// - Reduce number of querySelectors (global)
+// --- Create form globaly?
